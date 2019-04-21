71f76ff919447f6130070b47f640da213676b5e0
BXM <brennan913@gmail.com>
2019-04-21 16:33:44 -0400

    Add working basic chord functionality

---
+++ cs4995/chordTest.cpp
@@ -0,0 +1,27 @@
+#include "MidiOutput.hpp"
+#include "Note.hpp"
+#include "Key.hpp"
+#include "Track.hpp"
+
+using namespace smf;
+
+int main() {
+
+    // create tracks
+    Track melody;
+    //melody << "C C G G A A G . F F E E D D C -";
+    //bass << "C3 C4 E4 C4 F4 C4 E4 C4 D4 B3 C4 A3 F3 G3 C3 -";
+    melody << "C/E/G - G/B/D - A/C/E - F/A/C -";
+
+
+    // combine tracks
+    MidiOutput out;
+    out.addTrack(melody);
+
+    // write tracks
+    out.write("chords.mid");
+
+
+
+    return 0;
+}
