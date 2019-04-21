d67e70083f9db294bce8f0697ea9dfe4cc6ba46e
BXM <brennan913@gmail.com>
2019-04-21 18:59:59 -0400

    Add first draft of chord string splitter

--- cs4995/StringProcessing.hpp
+++ cs4995/StringProcessing.hpp
@@ -20,4 +20,29 @@ vector<string> tokenize(string str, char delimiter) {
     return tokens;
 }
 
+// returns true if c is one of
+// {'A','B','C','D','E','F','G'}
+bool is_note_char(char& c){
+    return (c >= 'A' && c <= 'G');
+}
+
+// assumes valid input
+// creates new token every time it encounters one of
+// {'A','B','C','D','E','F','G'}
+vector<string> tokenize_chordstr(string str){
+    vector<string> tokens;
+    unsigned int start, end;
+    end = 0;
+    while (end < str.size()){
+	start = end;
+	while ( !(is_note_char(str[end])) && end < str.size() ) {
+	    end++;
+	}
+
+	tokens.push_back(str.substr(start, end-start));
+	
+    return tokens;
+
+    }
+}
 #endif
