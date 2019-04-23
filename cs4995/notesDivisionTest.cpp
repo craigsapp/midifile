98a1ec120034f0835ed2c228ccd4773566452db9
Conder <cs3544@columbia.edu>
2019-04-22 21:18:01 -0400

    msg

--- cs4995/notesDivisionTest.cpp
+++ cs4995/notesDivisionTest.cpp
@@ -9,8 +9,8 @@ int main() {
 
     // create tracks
     Track melody;
-    melody << "4( C E G F ) 1( E ) 8( E/G/B E/G/B E G E D C C ) 2( A )";
-
+    // melody << "4( C E G F ) 1( E ) 8( E/G/B E/G/B E G E D C C ) 2( A )";
+    melody << "4(C E G F) 1(E)";
 
     // combine tracks
     MidiOutput out;
