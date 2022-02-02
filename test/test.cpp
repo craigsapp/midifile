f48cf5eb09f700685052343a8bfbb9091edfc7ad
Nathan Ho <nathan.waikin.ho@gmail.com>
2022-02-02 15:00:08 -0800

    Add first test

--- test/test.cpp
+++ test/test.cpp
@@ -1,2 +1,18 @@
+#include <sstream>
+
 #define CATCH_CONFIG_MAIN
 #include "catch2/catch.hpp"
+
+#include "MidiFile.h"
+
+TEST_CASE("Writing and reading an empty MIDI file works") {
+    smf::MidiFile midifile1;
+    REQUIRE(midifile1.size() == 1);
+    std::stringstream stream;
+    midifile1.write(stream);
+    REQUIRE(midifile1.status());
+    smf::MidiFile midifile2;
+    midifile2.read(stream);
+    REQUIRE(midifile2.size() == 1);
+    REQUIRE(midifile2.status());
+}
