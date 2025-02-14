3a0ff5d52091d3845db38a262c046c1664c40291
Craig Stuart Sapp <craigsapp@gmail.com>
2025-02-13 21:28:24 -0800

    Make event sorting function more clear.

--- src/MidiEventList.cpp
+++ src/MidiEventList.cpp
@@ -548,31 +548,38 @@ void MidiEventList::sort(void) {
 //    (4) note-offs come after all other regular MIDI messages except note-ons.
 //    (5) note-ons come after all other regular MIDI messages.
 //
+// Note: If you load a MIDI file from a file, MidiMessage.seq numbers
+// will automatically be added, and sorting will follow the sequence
+// numbers.  Use MidiFile::clearSequence() if you want to sort the
+// MIDI events (occuring at the same time) using the rest of the sorting
+// algorithm (such as to place note-offs before note-ons when they
+// occur at the same time).
+//
 
 int eventcompare(const void* a, const void* b) {
 	MidiEvent& aevent = **((MidiEvent**)a);
 	MidiEvent& bevent = **((MidiEvent**)b);
 
-	if (aevent.tick > bevent.tick) {
-		// aevent occurs after bevent
-		return +1;
-	} else if (aevent.tick < bevent.tick) {
+	if (aevent.tick < bevent.tick) {
 		// aevent occurs before bevent
 		return -1;
-	} else if ((aevent.seq != 0) && (bevent.seq != 0) && (aevent.seq > bevent.seq)) {
-		// aevent sequencing state occurs after bevent
-		// see MidiEventList::markSequence()
+	} else if (aevent.tick > bevent.tick) {
+		// aevent occurs after bevent
 		return +1;
 	} else if ((aevent.seq != 0) && (bevent.seq != 0) && (aevent.seq < bevent.seq)) {
 		// aevent sequencing state occurs before bevent
 		// see MidiEventList::markSequence()
 		return -1;
-	} else if (aevent.getP0() == 0xff && aevent.getP1() == 0x2f) {
-		// end-of-track meta-message should always be last (but won't really
+	} else if ((aevent.seq != 0) && (bevent.seq != 0) && (aevent.seq > bevent.seq)) {
+		// aevent sequencing state occurs after bevent
+		// see MidiEventList::markSequence()
+		return +1;
+	} else if ((aevent.getP0() == 0xff) && (aevent.getP1() == 0x2f)) {
+		// end-of-track meta-message should always be last (but this won't really
 		// matter since the writing function ignores all end-of-track messages
 		// and writes its own.
 		return +1;
-	} else if (bevent.getP0() == 0xff && bevent.getP1() == 0x2f) {
+	} else if ((bevent.getP0() == 0xff) && (bevent.getP1() == 0x2f)) {
 		// end-of-track meta-message should always be last (but won't really
 		// matter since the writing function ignores all end-of-track messages
 		// and writes its own.
@@ -580,29 +587,54 @@ int eventcompare(const void* a, const void* b) {
 	} else if (aevent.getP0() == 0xff && bevent.getP0() != 0xff) {
 		// other meta-messages are placed before real MIDI messages
 		return -1;
-	} else if (aevent.getP0() != 0xff && bevent.getP0() == 0xff) {
+	} else if ((aevent.getP0() != 0xff) && (bevent.getP0() == 0xff)) {
 		// other meta-messages are placed before real MIDI messages
 		return +1;
-	} else if (((aevent.getP0() & 0xf0) == 0x90) && (aevent.getP2() != 0)) {
+	} else if ((aevent.getP0() == 0xff) && (bevent.getP0() == 0xff)) {
+		// could sort by meta-message event number here
+		return 0;
+	} else if (aevent.isNote() && !bevent.isNote()) {
+		// notes come after all other message types
+		return +1;
+	} else if (!aevent.isNote() && bevent.isNote()) {
+		// notes come after all other message types
+		return -1;
+	} else if (aevent.isNoteOff() && bevent.isNoteOn()) {
+		// note-offs come before note-ons
+		return -1;
+	} else if (aevent.isNoteOn() && bevent.isNoteOff()) {
 		// note-ons come after all other types of MIDI messages
 		return +1;
-	} else if (((bevent.getP0() & 0xf0) == 0x90) && (bevent.getP2() != 0)) {
-		// note-ons come after all other types of MIDI messages
-		return -1;
-	} else if (((aevent.getP0() & 0xf0) == 0x90) || ((aevent.getP0() & 0xf0) == 0x80)) {
-		// note-offs come after all other MIDI messages (except note-ons)
-		return +1;
-	} else if (((bevent.getP0() & 0xf0) == 0x90) || ((bevent.getP0() & 0xf0) == 0x80)) {
-		// note-offs come after all other MIDI messages (except note-ons)
-		return -1;
+	} else if (aevent.isNoteOn() && bevent.isNoteOn()) {
+		// sorted further by key number
+		if (aevent.getP1() < bevent.getP1()) {
+			return -1;
+		} else if (aevent.getP1() > bevent.getP1()) {
+			return +1;
+		} else {
+			return 0;
+		}
+		return 0;
+	} else if (aevent.isNoteOff() && bevent.isNoteOff()) {
+		// sorted further by key number
+		if (aevent.getP1() < bevent.getP1()) {
+			return -1;
+		} else if (aevent.getP1() > bevent.getP1()) {
+			return +1;
+		} else {
+			return 0;
+		}
+		// could be sorted further by key number.
+		return 0;
 	} else if (((aevent.getP0() & 0xf0) == 0xb0) && ((bevent.getP0() & 0xf0) == 0xb0)) {
-		// both events are continuous controllers.  Sort them by controller number
+		// both events are continuous controllers.  Sort them by controller number and value if equal
 		if (aevent.getP1() > bevent.getP1()) {
 			return +1;
 		} if (aevent.getP1() < bevent.getP1()) {
 			return -1;
 		} else {
 			// same controller number, so sort by data value
+			// useful for sustain pedalling, for example.
 			if (aevent.getP2() > bevent.getP2()) {
 				return +1;
 			} if (aevent.getP2() < bevent.getP2()) {
